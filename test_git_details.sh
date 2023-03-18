50a917beaa581f462ab1521d6e2ff6360174aa06
Emmanuel Ubuntu <emmanuel@ubuntu>
2023-03-18 08:51:50 -0400

    will use other poc instead

---
+++ test_git_details.sh
@@ -0,0 +1,11 @@
+GIT_NEW_REPO_NAME=keptndemo5
+GIT_USER=emmanuelknafodevopsabcs
+GITHUB_TOKEN=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
+export GIT_REPO=https://github.com/$GIT_USER/$GIT_NEW_REPO_NAME.git
+export GIT_USER=$GIT_USER
+export GITHUB_TOKEN=$GITHUB_TOKEN
+export GIT_NEW_REPO_NAME=$GIT_NEW_REPO_NAME
+echo "git repo $GIT_REPO"
+echo "name $GIT_NEW_REPO_NAME"
+echo "user $GIT_USER"
+echo "token $GITHUB_TOKEN"
